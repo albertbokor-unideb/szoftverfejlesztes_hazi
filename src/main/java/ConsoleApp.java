252d6018e9262c9a11addea8103df7917f74762b
albertbokor-unideb <128703802+albertbokor-unideb@users.noreply.github.com>
2023-05-31 22:48:51 +0300

    minor cosmetic change

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 public class ConsoleApp {
 
     public static void main(String[] args) {
-        System.out.print("Egy 4 × 4 mezőből  álló tábla mezőire 16 darab kavicsot helyezünk, minden\n" +
-                "mezőre egyet-egyet. A két játékos felváltva vehet le a tábláról kavicsokat,\n" +
-                "legalább 1 és legfeljebb 4 darabot. Csak olyan kavicsokat lehet levenni, \n" +
-                "amelyek egy sorban vagy egy oszlopban vannak,  és nincs közöttük üres mező. Az\n" +
-                "veszít, aki utoljára lép.\n\n"+
-                "a kiválasztott mezőket `x1 y1 x2 y2...` formában adja meg (x=oszlop, y=sor)!\n" +
-                "kilépni `q`-val tud\n");
+        System.out.print("""
+                Egy 4 × 4 mezőből  álló tábla mezőire 16 darab kavicsot helyezünk, minden
+                mezőre egyet-egyet. A két játékos felváltva vehet le a tábláról kavicsokat,
+                legalább 1 és legfeljebb 4 darabot. Csak olyan kavicsokat lehet levenni,\s
+                amelyek egy sorban vagy egy oszlopban vannak,  és nincs közöttük üres mező. Az
+                veszít, aki utoljára lép.
+
+                a kiválasztott mezőket `x1 y1 x2 y2...` formában adja meg (x=oszlop, y=sor)!
+                kilépni `q`-val tud
+                """);
         var model = new BoardGameModel();
 
         Scanner input = new Scanner(System.in);
@@ -29,7 +32,7 @@ public class ConsoleApp {
                     System.exit(0);
                 }
                 Scanner lineScanner = new Scanner(line);
-                ArrayList<position> coordinateList = new ArrayList<position>();
+                ArrayList<position> coordinateList = new ArrayList<>();
                 int newRow;
                 int newCol;
                 while(lineScanner.hasNextInt()){
