bbda75c0b8686d623cc7fdcaeb8fb8fe0e906ba9
albertbokor-unideb <128703802+albertbokor-unideb@users.noreply.github.com>
2023-05-31 22:47:46 +0300

    Added exit option

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -1,11 +1,10 @@
 import model.BoardGameModel;
-import model.Player;
 import model.position;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class ConsoleApp {
 
     public static void main(String[] args) {
         System.out.print("Egy 4 × 4 mezőből  álló tábla mezőire 16 darab kavicsot helyezünk, minden\n" +
@@ -13,7 +12,8 @@ public class Main {
                 "legalább 1 és legfeljebb 4 darabot. Csak olyan kavicsokat lehet levenni, \n" +
                 "amelyek egy sorban vagy egy oszlopban vannak,  és nincs közöttük üres mező. Az\n" +
                 "veszít, aki utoljára lép.\n\n"+
-                "a kiválasztott mezőket `x1 y1 x2 y2...` formában adja meg (x=oszlop, y=sor)!\n\n");
+                "a kiválasztott mezőket `x1 y1 x2 y2...` formában adja meg (x=oszlop, y=sor)!\n" +
+                "kilépni `q`-val tud\n");
         var model = new BoardGameModel();
 
         Scanner input = new Scanner(System.in);
@@ -25,6 +25,9 @@ public class Main {
                     case PLAYER_2 -> System.out.println("Játékos_2: ");
                 }
                 String line = input.nextLine();
+                if(line.equalsIgnoreCase("q")){
+                    System.exit(0);
+                }
                 Scanner lineScanner = new Scanner(line);
                 ArrayList<position> coordinateList = new ArrayList<position>();
                 int newRow;
