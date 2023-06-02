25911b9221265b7daf68cc9fd545a6bf1e3933f3
albertbokor-unideb <128703802+albertbokor-unideb@users.noreply.github.com>
2023-06-02 14:37:17 +0300

    added game rules text to ui.fxml

--- src/main/java/boardgame/ConsoleApp.java
+++ src/main/java/boardgame/ConsoleApp.java
@@ -12,7 +12,7 @@ public class ConsoleApp {
         System.out.print("""
                 Egy 4 × 4 mezőből  álló tábla mezőire 16 darab kavicsot helyezünk, minden
                 mezőre egyet-egyet. A két játékos felváltva vehet le a tábláról kavicsokat,
-                legalább 1 és legfeljebb 4 darabot. Csak olyan kavicsokat lehet levenni,\s
+                legalább 1 és legfeljebb 4 darabot. Csak olyan kavicsokat lehet levenni,
                 amelyek egy sorban vagy egy oszlopban vannak,  és nincs közöttük üres mező. Az
                 veszít, aki utoljára lép.
 
